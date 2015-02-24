1017bf7e0ec386fbe690c2f93700e00d5887038e
Zabrina <zabrinaisis@gmail.com>
2015-02-23 20:59:32 -0600

    Updates to FizzBuzz.java
    Added an if statement to print a one line "Fizz Buzz!" instead of two
    lines.

--- W1D1/FizzBuzz.java
+++ W1D1/FizzBuzz.java
@@ -4,17 +4,21 @@ public class FizzBuzz {
 		
 		while (num <= 100) {
 			
-			if (num % 3 == 0) {
-				System.out.println("Fizz");					// prints if divisible by 3
+			if (num % 3 == 0 && num % 5 == 0) {
+				System.out.println("Fizz Buzz!");					// prints if divisible by both 3 and 5
+			} else {
+				if (num % 3 == 0) {
+					System.out.println("Fizz!");					// prints if divisible by 3
+				}
+				if (num % 5 == 0) {
+					System.out.println("Buzz!");					// prints if divisible by 5
+				}
+				if (num % 3 != 0 && num % 5 != 0) {
+					System.out.println(num);						// prints all other numbers
+				}
 			}
-			if (num % 5 == 0) {
-				System.out.println("Buzz");					// prints if divisible by 5
-			}
-			if (num % 3 != 0 && num % 5 != 0) {
-				System.out.println(num);					// prints all other numbers
-			}
-			
-			num = num + 1;									// keeps numbers moving toward 100
+						
+			num = num + 1;											// keeps numbers moving toward 100
 			
 		} // end while
 	} // end main
