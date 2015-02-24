3db9105de0bf822707cdc197a2f72ffe609f9557
zabrinaisis <zabrinaisis@gmail.com>
2015-02-24 15:34:25 -0600

    Creates a Dog object.
    Variables for size, breed, and dog's name. Method for bark.

---
+++ W1D2/Dog.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package w1d2;
+
+/**
+ *
+ * @author Zbrodale
+ */
+class Dog {
+    
+    int size;
+    String breed;
+    String name;
+    
+    void bark() {
+        System.out.println("Ruff Ruff");
+    }
+}
