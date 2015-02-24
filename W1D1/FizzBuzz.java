a4bbcf682e2c8b3ccebe9c8af19180eebaa47736
Zabrina <zabrinaisis@gmail.com>
2015-02-23 21:06:06 -0600

    Updates to FizzBuzz.java
    Changed the while loop to a for loop.

--- W1D1/FizzBuzz.java
+++ W1D1/FizzBuzz.java
@@ -1,8 +1,6 @@
 public class FizzBuzz {
 	public static void main (String[] args) {
-		int num = 1;
-		
-		while (num <= 100) {
+		for (int num = 1; num <=100; num++) {
 			
 			if (num % 3 == 0 && num % 5 == 0) {
 				System.out.println("Fizz Buzz!");					// prints if divisible by both 3 and 5
@@ -16,10 +14,31 @@ public class FizzBuzz {
 				if (num % 3 != 0 && num % 5 != 0) {
 					System.out.println(num);						// prints all other numbers
 				}
+			}	
+		} // end for
+	} // end main
+} // end class
+
+/* 
+public class FizzBuzz {
+	public static void main (String[] args) {
+		int num = 1;
+		while (num <= 100) {
+			if (num % 3 == 0 && num % 5 == 0) {
+				System.out.println("Fizz Buzz!");					// prints if divisible by both 3 and 5
+			} else {
+				if (num % 3 == 0) {
+					System.out.println("Fizz!");					// prints if divisible by 3
+				}
+				if (num % 5 == 0) {
+					System.out.println("Buzz!");					// prints if divisible by 5
+				}
+				if (num % 3 != 0 && num % 5 != 0) {
+					System.out.println(num);						// prints all other numbers
+				}
 			}
-						
 			num = num + 1;											// keeps numbers moving toward 100
-			
 		} // end while
 	} // end main
 } // end class
+*/
