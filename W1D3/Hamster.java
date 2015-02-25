6f231f5dce73ff74de603a0e591aa273f9f7b654
zabrinaisis <zabrinaisis@gmail.com>
2015-02-25 15:52:49 -0600

    Creates a Hamster object.
    Creates a Hamster object with an age in double form.

---
+++ W1D3/Hamster.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package w1d3;
+
+/**
+ *
+ * @author Zbrodale
+ */
+public class Hamster {
+    
+    public double age;
+    
+}
