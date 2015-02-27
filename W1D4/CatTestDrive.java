a040595fadec95de6e4fd9e40fa1739cb3b1fcf2
Zabrina <zabrinaisis@gmail.com>
2015-02-27 09:41:55 -0600

    Prints values of Cat object.
    Prints the default value of the Cat object.

---
+++ W1D4/CatTestDrive.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d4;
+
+/**
+ *
+ * @author ZBrodale
+ */
+public class CatTestDrive {
+    public static void main(String[] args) {
+        Cat Schrodinger = new Cat();
+        System.out.println("Schrodinger's name is: " + Schrodinger.getName());
+        System.out.println("Schrodinger's gender is: " + Schrodinger.getGender());
+        System.out.println("Schrodinger's age is: " + Schrodinger.getAge());
+    }
+}
