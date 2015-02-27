6d2f01d97a807ebb02369da5401409544ee43cc5
Zabrina <zabrinaisis@gmail.com>
2015-02-27 09:43:34 -0600

    Creates a GuineaPig object.
    Creates a GuineaPig object that has a private name.

---
+++ W1D4/GuineaPig.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d4;
+
+/**
+ *
+ * @author ZBrodale
+ */
+public class GuineaPig {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return this.name;
+    }
+    
+}
