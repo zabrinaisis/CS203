b1bc3d292068a9378d03ad2e4968c072af162174
zabrinaisis <zabrinaisis@gmail.com>
2015-02-27 11:07:05 -0600

    Creates a ScientistMichael object.
    Creates a ScientistMichael object that has a private name. Also includes
    a method that alters an int and a GuineaPigMichael.

---
+++ W1D4/ScientistMichael.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package other;
+
+/**
+ *
+ * @author Zbrodale
+ */
+public class ScientistMichael {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int changeThese(int subjectA, GuineaPigMichael subjectB) {
+        subjectA = 8;
+        subjectB.setName("Susan");
+        return subjectA;
+    }
+    
+}
