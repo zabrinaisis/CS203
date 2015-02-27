54364d3ac6f9932f4979fa491a08e7281fbb7ee0
Zabrina <zabrinaisis@gmail.com>
2015-02-27 09:41:19 -0600

    Creates a Cat object.
    Creates a cat object that has a name, gender, and age which are private.

---
+++ W1D4/Cat.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d4;
+
+/**
+ *
+ * @author ZBrodale
+ */
+public class Cat {
+    
+    private String name;
+    private String gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
