b67ab6ea38a0883f1574ad08231dd876aaab5328
Zabrina <zabrinaisis@gmail.com>
2015-03-02 09:35:23 -0600

    Simple Dot Come Game from Head First Java
    Exact game from the book, chapter 5.

---
+++ W1D6/SimpleDotComGame.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d6;
+
+/**
+ *
+ * @author ZBrodale
+ */
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses =0;
+        GameHelper helper = new GameHelper();
+        
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+        
+        while (isAlive == true) {
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            }
+        }
+    }
+}
