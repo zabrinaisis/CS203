2eb14b3acff917e3f9ec354a1d681eafbfdd63c3
Zabrina <zabrinaisis@gmail.com>
2015-03-02 20:48:36 -0600

    Sink a Dot Com Game
    Includes programs: DotCom, DotComBust, and GameHelper. Similar to the
    board game Battleship.

---
+++ W2D1/DotCom.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w2d1;
+import java.util.*;
+/**
+ *
+ * @author ZBrodale
+ */
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+    
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+    
+    public void setName(String n) {
+        name = n;
+    }
+    
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + "    : ( ");
+            } else {
+                result = "hit";
+            }
+        }
+        return result;
+    }
+}
